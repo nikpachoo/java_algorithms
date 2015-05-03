37c58d68ce8c3c1fa0c7ecaecab0b10a9270d814
nikpachoo <nikpachoo@gmail.com>
2015-05-03 21:10:56 +0300

    add solution exercise 1.1.3

---
+++ src/chapter1/_p1e3.java
@@ -0,0 +1,30 @@
+package chapter1;
+
+import libs.In;
+import libs.StdOut;
+
+/**
+ * exercise 3 from chapter 1 partial 1
+ */
+public class _p1e3 {
+    public static String compare(int[] arr)
+    {
+        int i = 0;
+        boolean isEqual = true;
+
+        while (isEqual && i < arr.length - 1)
+        {
+            isEqual = (arr[i] == arr[i + 1]);
+            ++i;
+        }
+
+        return isEqual ? "equal" : "not equal";
+    }
+
+    public static void main(String[] args)
+    {
+        int[] comparableList = In.readInts(args[0]);
+        String isEquals = compare(comparableList);
+        StdOut.println(isEquals);
+    }
+}
