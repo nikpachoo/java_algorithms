7d9df258a7e7d3443f29b9122082332ec4fbdb81
nikpachoo <nikpachoo@gmail.com>
2015-06-21 15:08:46 +0300

    add solution exercise 1.1.20

---
+++ src/chapter1/_p1e20.java
@@ -0,0 +1,46 @@
+package chapter1;
+
+/**
+ * exercise 20 from chapter 1 partial 1
+ * - compute the value of ln(N!)
+ */
+public class _p1e20 {
+    public static double lnOfFactorial (int N)
+    {
+        double factN = (double)factorial(N);
+        return Math.log(factN);
+    }
+
+    public static long factorial (int N)
+    {
+        if (N < 0)
+            return 0;
+        if (N == 0)
+            return 1;
+        if (N == 1 || N == 2)
+            return N;
+
+        return factorialTree(2, N);
+    }
+
+    private static long factorialTree (int l, int r)
+    {
+        if (l > r)
+            return 1;
+        if (l == r)
+            return l;
+        if (r - l == 1)
+            return (long) l * r;
+
+        int m = (l + r) / 2;
+
+        return factorialTree(l, m) * factorialTree(m + 1, r);
+    }
+
+    public static void main (String[] args)
+    {
+        int N = Integer.parseInt(args[0]);
+
+        System.out.println(lnOfFactorial(N));
+    }
+}
